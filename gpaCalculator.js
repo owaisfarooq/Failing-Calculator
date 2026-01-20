1c39a6d23df4a186000082c2685bc8a04d47361a
Ramz29 <rameezahmed066@gmail.com>
2026-01-20 13:21:47 +0500

    Add 3 courses at a time. Atleast one row should be present.

--- gpaCalculator.js
+++ gpaCalculator.js
@@ -1,40 +1,47 @@
 function addCourse() {
   const tbody = document.querySelector("tbody");
-  const newRow = document.createElement("tr");
-  newRow.innerHTML = `
-    <td>
-        <input class="form-control courseName" type="text" placeholder="Course" />
-    </td>
-    <td>
-        <input class="form-control creditHours" type="number" placeholder="Crdts" />
-    </td>
-    <td>
-        <select class="form-control grade">
-        <option>-</option>
-        <option value="4.0">A</option>
-        <option value="3.67">A-</option>
-        <option value="3.33">B+</option>
-        <option value="3.0">B</option>
-        <option value="2.67">B-</option>
-        <option value="2.33">C+</option>
-        <option value="2.0">C</option>
-        <option value="1.67">C-</option>
-        <option value="1.0">D</option>
-        <option value="0.0">F</option>
-        </select>
-    </td>
-    <td>
-        <button onclick="deleteCourse(this)" class="btn btn-secondary">
-        <i class="fa-solid fa-x"></i>
-        </button>
-    </td>
+  for (let i = 0; i < 3; i++) {
+    const newRow = document.createElement("tr");
+    newRow.innerHTML = `
+        <td>
+            <input class="form-control courseName" type="text" placeholder="Course" />
+        </td>
+        <td>
+            <input class="form-control creditHours" type="number" placeholder="Crdts" />
+        </td>
+        <td>
+            <select class="form-control grade">
+            <option>-</option>
+            <option value="4.0">A</option>
+            <option value="3.67">A-</option>
+            <option value="3.33">B+</option>
+            <option value="3.0">B</option>
+            <option value="2.67">B-</option>
+            <option value="2.33">C+</option>
+            <option value="2.0">C</option>
+            <option value="1.67">C-</option>
+            <option value="1.0">D</option>
+            <option value="0.0">F</option>
+            </select>
+        </td>
+        <td>
+            <button onclick="deleteCourse(this)" class="btn btn-secondary">
+            <i class="fa-solid fa-x"></i>
+            </button>
+        </td>
     `;
-  tbody.appendChild(newRow);
+    tbody.appendChild(newRow);
+  }
 }
 
 function deleteCourse(button) {
-  const row = button.closest("tr");
-  row.remove();
+  const rows = document.querySelectorAll("tbody tr");
+  if (rows.length > 1) {
+    const row = button.closest("tr");
+    row.remove();
+  }else{
+    alert("Atleast one row must be present.");
+  }
 }
 
 function calculateGPA() {
