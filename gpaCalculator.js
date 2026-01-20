e873c0109ebb1d406fe70c00902b229902429cbe
Ramz29 <rameezahmed066@gmail.com>
2026-01-20 13:05:09 +0500

    Added addCourse and updated calculateGPA

--- gpaCalculator.js
+++ gpaCalculator.js
@@ -1,5 +1,35 @@
 function addCourse() {
-
+  const tbody = document.querySelector("tbody");
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+    <td>
+        <input class="form-control courseName" type="text" placeholder="Course" />
+    </td>
+    <td>
+        <input class="form-control creditHours" type="number" placeholder="Crdts" />
+    </td>
+    <td>
+        <select class="form-control grade">
+        <option>-</option>
+        <option value="4.0">A</option>
+        <option value="3.67">A-</option>
+        <option value="3.33">B+</option>
+        <option value="3.0">B</option>
+        <option value="2.67">B-</option>
+        <option value="2.33">C+</option>
+        <option value="2.0">C</option>
+        <option value="1.67">C-</option>
+        <option value="1.0">D</option>
+        <option value="0.0">F</option>
+        </select>
+    </td>
+    <td>
+        <button onclick="deleteCourse()" class="btn btn-secondary">
+        <i class="fa-solid fa-x"></i>
+        </button>
+    </td>
+    `;
+  tbody.appendChild(newRow);
 }
 
 function deleteCourse() {}
@@ -7,14 +37,15 @@ function deleteCourse() {}
 function calculateGPA() {
   const rows = document.querySelectorAll("tbody tr");
   let gpa = 0;
-  let sgpa = 0;
   let totalCrdts = 0;
   rows.forEach((row) => {
-    const crdthrs = row.querySelector("#creditHours").value;
-    const grade = row.querySelector("#grade").value;
-    gpa += crdthrs * grade;
-    totalCrdts += crdthrs;
+    const crdthrs = Number(row.querySelector(".creditHours").value);
+    const grade = Number(row.querySelector(".grade").value);
+    if (grade && crdthrs) {
+      gpa += crdthrs * grade;
+      totalCrdts += crdthrs;
+    }
   });
-  sgpa = gpa / totalCrdts;
+  const sgpa = totalCrdts > 0 ? gpa / totalCrdts : 0;
   document.getElementById("gparesult").textContent = `GPA: ${sgpa.toFixed(2)}`;
 }
